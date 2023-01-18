107e1f5e9bb712b84f70049784289850d6205ef5
abhishekgadhia1 <abhishek.gadhia@gmail.com>
2023-01-18 22:48:30 +0530

    first commit

---
+++ src/main/java/com/practice/RegisterServlet.java
@@ -0,0 +1,58 @@
+package com.practice;
+
+import jakarta.security.auth.message.callback.PrivateKeyCallback.Request;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h2>Welcome to Register servlet</h2>");
+		
+		String name = request.getParameter("user_name");
+		String password = request.getParameter("user_password");
+		String email = request.getParameter("user_email");
+		String gender = request.getParameter("user_gender");
+		String course = request.getParameter("user_course");
+		String cond = request.getParameter("condition");
+		
+		if(cond!=null)
+		{
+		if(cond.equals("checked"))
+		{
+			out.println("Name : "+name);
+			out.println("<h2>Password : "+password+"</h2>");
+			out.println("Email : "+email);
+			out.println("Gender : "+gender);
+			out.println("Course : "+course);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("SuccessServlet");
+			rd.forward(request, response);
+		}
+		else
+		{
+			out.println("you have not agreed to t and c");
+		}
+		}
+		else
+		{
+			out.println("you have not agreed to t and c");
+			
+			RequestDispatcher rd = request.getRequestDispatcher("index.html");
+			rd.include(request, response);
+		}
+		
+	}
+
+}
---
+++ src/main/java/com/practice/SuccessServlet.java
@@ -0,0 +1,21 @@
+package com.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.rmi.ServerException;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SuccessServlet extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServerException, IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("This is success servlet");
+		out.println("<h2>Successfully registered</h2>");
+	}
+}
